a7bc6ec14c4fbb6760cc19e79418794d898a5579
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-01-20 08:31:09 +0000

    Adding collisions for treasure; reorganising
    - colisions between treasure and player controller result in the
    treasure being destroyed and a score counter being incremented

---
+++ Assets/scripts/player_collision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_collision : MonoBehaviour {
+
+	int treasure_collected = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.CompareTag ("Treasure")) {
+			col.gameObject.SetActive (false);
+			treasure_collected ++;
+		}
+	}
+}
